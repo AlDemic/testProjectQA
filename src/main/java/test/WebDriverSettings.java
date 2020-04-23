2331f8fd702b42ecbd083364b17bf8445f93c1bf
Alexandr <thealdemic@gmail.com>
2020-04-23 19:10:40 +0300

    QA test task - supply project to git by IntelliJ IDEA

---
+++ src/main/java/test/WebDriverSettings.java
@@ -0,0 +1,34 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.PageFactory;
+import pages.AfterSearchPage;
+import pages.MainPage;
+
+import java.util.concurrent.TimeUnit;
+
+public class WebDriverSettings {
+    public WebDriver driver;
+    public MainPage main;
+    public AfterSearchPage afterSearch;
+
+    @Before
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/just-/Downloads/chromedriver_win32/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        main = PageFactory.initElements(driver, MainPage.class);
+        afterSearch = PageFactory.initElements(driver, AfterSearchPage.class);
+    }
+
+    @After
+    public void finish() {
+        driver.quit();
+    }
+}
